50e306f17e519ff0c575d82ebb351d810956f6f1
Paulo Dutra <paulodutra.info@gmail.com>
2019-05-20 00:14:24 -0300

    removendo e renomeando pastas

---
+++ desafio-3/storage/framework/views/b2a7283402ad3090846f50eb885809b86210b573.php
@@ -0,0 +1,43 @@
+<?php $__env->startSection('content'); ?>
+<?php if($errors->any()): ?>
+    <div class="alert alert-danger">
+        <ul>
+            <?php $__currentLoopData = $errors->all(); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $error): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                <li><?php echo e($error); ?></li>
+            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+        </ul>
+    </div>
+<?php endif; ?>
+<form class="form-horizontal" method="post" action="<?php echo e(url('usuarios/atualizar', $usuario->id)); ?>">
+  <?php echo method_field('PUT'); ?>
+  <?php echo e(csrf_field()); ?>
+
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="nome">Nome</label>
+      <input type="text" class="form-control" name="nome" id="nome" value=<?php echo e($usuario->nome); ?> placeholder="Nome">
+    </div>
+    <div class="form-group col-md-6">
+      <label for="email">Email</label>
+      <input type="email" class="form-control" name="email" id="email" value=<?php echo e($usuario->email); ?> placeholder="Email">
+    </div>
+  </div>
+  <div class="form-row">
+    <div class="form-group col-md-4">
+      <label for="data_nascimento">Data de nascimento</label>
+      <input type="text" class="form-control" name="data_nascimento" id="data_nascimento" value=<?php echo e(date('d/m/Y', strtotime($usuario->data_nascimento))); ?> placeholder="Data de nascimento">
+    </div>
+    <div class="form-group col-md-4">
+      <label for="senha">Senha</label>
+      <input type="password" class="form-control" name="senha"  id="senha" placeholder="Senha">
+    </div>
+    <div class="form-group col-md-4">
+      <label for="confirma_senha">Confirmação de senha</label>
+      <input type="password" class="form-control" name="confirma_senha" id="confirma_senha" placeholder="Confirmação de senha">
+    </div>
+  </div>
+  <button class="btn btn-success" type="submit">Salvar</button>
+</form>
+
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('templates.template', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /var/www/html/processo_seletivo_2019/desafio-3-laravel/resources/views/usuarios/update.blade.php ENDPATH**/ ?>
---
+++ desafio-3/storage/framework/views/bd407b822d14cb14b7f15af25fd3f37d91ce20c8.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
+
+    <title><?php echo e(config('app.name', 'Laravel')); ?></title>
+
+    <!-- Scripts -->
+    <script src="<?php echo e(asset('js/app.js')); ?>" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <link href="<?php echo e(asset('css/app.css')); ?>" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-dark bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="<?php echo e(url('/')); ?>">
+                    Desafio 3 - CNM
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="<?php echo e(__('Toggle navigation')); ?>">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+                       <li class="nav-item">
+                            <a class="nav-link" href="">listagem</a>
+                        </li>
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                       <li class="nav-item">
+                            <a class="nav-link" href="">Cadastro</a>
+                        </li>
+                    
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <main class="py-4">
+            <?php echo $__env->yieldContent('content'); ?>
+        </main>
+    </div>
+</body>
+</html>
+<?php /**PATH /var/www/html/processo_seletivo_2019/desafio-3-laravel/resources/views/layouts/app.blade.php ENDPATH**/ ?>
